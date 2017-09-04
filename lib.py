9334b0ae452acfcf4d9281b9ff0a2f0dd6e33f94
Marco Antonio <ma.souza.junior@gmail.com>
2017-09-04 15:57:42 -0300

    ENH : log

--- lib.py
+++ lib.py
@@ -5,9 +5,12 @@ from gpiozero import LED
 def toggle_light(ref):
     # Get leds
     lights = {}
-    lights["yellow"]  = { "led": LED(4),    "status": False }
-    lights["green"]   = { "led": LED(14),   "status": False }
-    lights["red"]     = { "led": LED(15),   "status": False }
+    lights["yellow"].led = LED(4)
+    lights["yellow"].status = False
+    lights["green"].led = LED(14)
+    lights["green"].status = False
+    lights["red"].led = LED(15)
+    lights["red"].status = False
 
     # If not string, throw error
     if (type(ref) != type('')):
@@ -15,20 +18,23 @@ def toggle_light(ref):
 
     # Yellow led
     if ref in ["yellow", "amarelo"]:
+        print("yellow")
         ref = "yellow"
 
     # Green led
     elif ref in ["green", "verde"]:
+        print("green")
         ref = "green"
 
     # Red led
     elif ref in ["red", "vermelho"]:
+        print("red")
         ref = "red"
 
     # Light on/off & Change Status
-    if lights[ref]["status"]:
-        lights[ref]["led"].off()
-        lights[ref]["status"] = False
+    if lights[ref].status:
+        lights[ref].led.off()
+        lights[ref].status = False
     else:
-        lights[ref]["led"].on()
-        lights[ref]["status"] = True
+        lights[ref].led.on()
+        lights[ref].status = True
